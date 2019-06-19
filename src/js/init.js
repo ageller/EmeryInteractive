483fdd9a93eaae7e49e41e6b43fd52a163e4d6c8
Aaron Geller <ageller@williamson.local>
2019-06-19 12:20:44 -0500

    added IP address to analytics

--- src/js/init.js
+++ src/js/init.js
@@ -77,6 +77,7 @@ function timeStamp() {
 
 
 //for IP addresses for google analytics
+//https://ourcodeworld.com/articles/read/257/how-to-get-the-client-ip-address-with-javascript-only
 function getIP(json) {
 	params.userIP = json.ip;
 	console.log("IP address : ", params.userIP)
